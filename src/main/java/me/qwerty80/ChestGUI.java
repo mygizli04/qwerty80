d5c74bdcc7fbdaf24fd27876ee3d1d3bb58086e6
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-08-12 00:21:33 +0400

    Implement item drops once a player closes a chest, add LootTable#forEachCategory, implement super duper secret debug command (might not work)
    Co-authored-by: JusteyCrustey <JusteyCrustey@users.noreply.github.com>

--- src/main/java/me/qwerty80/ChestGUI.java
+++ src/main/java/me/qwerty80/ChestGUI.java
@@ -1,14 +1,21 @@
 package me.qwerty80;
 
+import java.util.ArrayList;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 
 public class ChestGUI implements Listener {
+
+    ArrayList<PlayerInventory> inventories = new ArrayList<PlayerInventory>();
 
     boolean isChest(Material type) {
         return (type == Material.CHEST) || (type == Material.ENDER_CHEST);
@@ -37,6 +44,37 @@ public class ChestGUI implements Listener {
 
             event.getPlayer().openInventory(inventory);
             event.getClickedBlock().setType(Material.AIR);
+
+            inventories.add(new PlayerInventory(event.getPlayer(), inventory));
         }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        inventories.forEach((inventory) -> {
+            if (inventory.player == event.getPlayer()) {
+                inventory.inventory.forEach((item) -> {
+                    if (item == null) {
+                        return;
+                    }
+                    event.getPlayer().getWorld().dropItem(event.getPlayer().getTargetBlock(null, 5).getLocation().add(new Location(event.getPlayer().getWorld(), 0, 1, 0)), item);
+                });
+
+                try {
+                    inventories.remove(inventory);
+                }
+                catch (Exception err) {}
+            }
+        });
+    }
+}
+
+class PlayerInventory {
+    Player player;
+    Inventory inventory;
+
+    public PlayerInventory(Player _player, Inventory _inventory) {
+        player = _player;
+        inventory = _inventory;
     }
 }
