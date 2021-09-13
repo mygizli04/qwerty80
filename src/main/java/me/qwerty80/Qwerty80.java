a14dd098d29144f096116c4bee728c1babb0ea09
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-09-13 15:14:05 +0400

    Add debug mode

--- src/main/java/me/qwerty80/Qwerty80.java
+++ src/main/java/me/qwerty80/Qwerty80.java
@@ -6,10 +6,55 @@ import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.kyori.adventure.text.Component;
+
+enum DebugMode { // Where to send debug messages to
+    DISABLED, // Debug messages are ignored
+    CONSOLE, // Debug messages will be sent to the console
+    PLAYER, // Debug messages will be sent to the player if appropriate (or else it will be broadcasted)
+    BROADCAST // Broadcast all debug messages
+}
 
 // Main class
 public class Qwerty80 extends JavaPlugin {
+
+    private final DebugMode debugMode = DebugMode.DISABLED; // Change this to enable debug mode!
+    private final String[] disableDebugMode = new String[]{}; // Add things to here to disable debug mode from senders! For example to not get debug messages from DiamondPickaxe: new String[]{"DiamondPickaxe", "something else", "etc"}
+
+    @SuppressWarnings("unused") // no idea why it think that some code is unreachable, will find out in the future i guess!
+    public void debug(String message, Player player, String sender) {
+
+        String prefix = "[DEBUG] [" + sender + "] ";
+        prefix += (player != null) && (debugMode == DebugMode.PLAYER) ? "" : "[" + player.getName() + "] "; // removing this line gets rid of the dead code warning
+        
+        if (Utils.arrayContains(sender, disableDebugMode)) {
+            return;
+        }
+
+        switch (debugMode) {
+            case DISABLED:
+                // Ignore
+                break;
+            case CONSOLE:
+                getLogger().info(prefix + message);
+                break;
+            case PLAYER:
+                if (player != null) {
+                    player.sendMessage(Component.text(prefix + message));
+                }
+                else {
+                    getServer().broadcast(Component.text(prefix + message));
+                }
+                break;
+            case BROADCAST:
+                getServer().broadcast(Component.text(message));
+                
+        }
+    }
+
     public Stack<Game> games = new Stack<Game>(); // This stack will have every game instance.
     public CustomTeams teams = new CustomTeams(this);
 
