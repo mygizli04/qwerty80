42023895c4f64cd3f76b9c368145d82d40c44cc1
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-10-15 23:20:05 +0400

    Make file for the future team leave command

---
+++ src/main/java/me/qwerty80/commands/Escape/Team/EscapeTeamLeave.java
@@ -0,0 +1,12 @@
+package me.qwerty80.commands.Escape.Team;
+
+import me.qwerty80.commands.EscapeCommand;
+
+public class EscapeTeamLeave extends EscapeCommand {
+    public EscapeTeamLeave() {
+        super();
+        super.usage = "/escape team leave";
+    }
+
+    
+}
